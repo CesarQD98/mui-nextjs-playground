1001aa15e1d57bd8ed58c09253f9a5e238867985
Rico98 <asd.cqd98@gmail.com>
2023-10-29 23:32:31 -0500

    Added video monitor interface

---
+++ app/interfaz/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Interfaz de cliente",
+  description: "Interfaz de cliente",
+}
+
+export default function ClientInterfaceLayout({ children }) {
+  return (
+    <>{children}</>
+  )
+}

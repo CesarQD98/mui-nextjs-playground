9b0dbdabb65c44118ea935758a7e1d479210750e
Rico98 <asd.cqd98@gmail.com>
2023-12-08 21:27:43 -0500

    Updated main content of home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import './global.css'
 export const metadata = {
   title: 'Home Page',
   description: 'MUI + Next.js Playground'
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,20 @@
-import { Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import CreateForm from './components/CreateForm'
+import NewItem from './components/NewItem'
 
 export default function Home () {
   return (
     <main>
-      <Typography variant='h1'>Home Page</Typography>
+      <Typography variant='h1'>MUI + NextJS Playground</Typography>
+      <Typography variant='h2'>Here are some links to the components or stuff I've been building and learning 👋</Typography>
+
+      <Container>
+        <NewItem href='dashboard' description='Making up a basic Dashboard layout' />
+        <NewItem href='about-layouts' description='NextJS Layout docs' />
+        <NewItem href='interfaz' description='Antamina basic interface - POC' />
+        <NewItem href='testing-grid' description='Playing with MUI Grid component' />
+      </Container>
+
       <Typography variant='h2'>Simple Form</Typography>
 
       <CreateForm />
